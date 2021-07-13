c6ead95dc39d3ccd2d0d26657ab26f3ec4f6e663
Vinicius <vinicius@seo.com.br>
2021-07-13 16:11:20 -0300

    mudado a posicao da matriz para a posicao do tabuleiro

---
+++ src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package chess;
+
+/**
+ * Created by desenv6 on 13/07/2021.
+ */
+public class ChessException extends RuntimeException{
+    public ChessException(String s) {
+        super(s);
+    }
+}
