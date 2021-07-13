f30c56624d0c8461d7ef6d8fa3267bb6e01586b2
Vinicius <vinicius@seo.com.br>
2021-07-13 17:14:18 -0300

    feita a movimentacao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,30 @@ package application;
 
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 /**
  * Created by desenv6 on 13/07/2021.
  */
 public class Program {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-       UI.printBoat(chessMatch.getPieces());
+        while (true){
+            UI.printBoat(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition souce = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(souce, target);
+        }
+        //sc.close();
     }
 }
--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,9 +1,11 @@
 package chess;
 
+import boardgame.BoardException;
+
 /**
  * Created by desenv6 on 13/07/2021.
  */
-public class ChessException extends RuntimeException{
+public class ChessException extends BoardException{
     public ChessException(String s) {
         super(s);
     }
