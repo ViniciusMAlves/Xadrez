73d63aea211611dec22acf131eeffb600b7bd2a2
Vinicius <vinicius@seo.com.br>
2021-07-13 11:58:31 -0300

    Criada a classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 /**
  * Created by desenv6 on 13/07/2021.
  */
 public class Program {
     public static void main(String[] args){
-        
+       
     }
 }
